737b613170fe341c24e28888cc21bd88854833e8
Tomche Delev <tomce.delev@gmail.com>
2015-08-23 21:52:35 +0200

    Add google analytics

--- app/app.js
+++ app/app.js
@@ -1,5 +1,11 @@
 (function () {
   'use strict';
-  angular.module('taximk', ['ui.router', 'taximk.config', 'taximk.data']);
-
+  angular.module('taximk', ['ui.router', 'taximk.config', 'taximk.data'])
+    .run(function ($rootScope, $location, $window) {
+      $rootScope.$on('$stateChangeSuccess', function (event) {
+        if (!$window.ga)
+          return;
+        $window.ga('send', 'pageview', {page: $location.path()});
+      });
+    });
 })();
